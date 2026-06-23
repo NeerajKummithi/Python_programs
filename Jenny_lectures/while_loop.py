6ead2c7b3055ffd2bc2770fbefbad3dc90bcd3b2
NeerajKummithi <neerajkummithi1253@gmail.com>
2026-06-23 11:47:59 +0530

    23/6/26

---
+++ Jenny_lectures/while_loop.py
@@ -0,0 +1,13 @@
+# count=1
+# while count<=7:
+#     print(count)
+#     count+=1
+
+import random
+
+count=0
+while count<0.5:
+    count=random.random()
+    print(count)
+else:
+    print("else")    
